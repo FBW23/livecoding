76b587ef8371e58349d299deb6f9a2dbb8ec7ea8
Martina Freundorfer <martina.freundorfer@digitalcareerinstitute.org>
2019-12-18 10:27:45 +0100

    hello

---
+++ javascript/decisions.js
@@ -0,0 +1,33 @@
+// IF a > b 
+//     THEN return a;
+// IF b > a 
+//     THEN return b; 
+
+let a = 52;
+let b = 35;
+// Condition boolean statement
+if (a > b) {
+    // what should happen if the condition is true? 
+    console.log(a);
+} else {
+    // what should happen if the condition is false? 
+    console.log(b);
+}
+
+let c = 50;
+let d = 51;
+if (c === d) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+let z = 50;
+let m = 100;
+if (z > m) {
+    console.log(true);
+} else { 
+    // Default case 
+    console.log(false);
+}
+
