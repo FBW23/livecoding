7014f2210db7f7ae252e7a46621804e2a8f78148
Martina Freundorfer <martina.freundorfer@digitalcareerinstitute.org>
2020-01-21 13:05:52 +0100

    hello

--- javascript/repetition-switch.js
+++ javascript/repetition-switch.js
@@ -201,7 +201,7 @@ function roundString2(inputString2) {
         switch (currentChar) {
             case ' ':
             case '@':
-            case 1: 
+            case 1:
             case '_':
             case '#': // become creative => Regular Expressions to the rescue! 
                 // Do nothing! must be the first 
@@ -227,3 +227,42 @@ function roundString2(inputString2) {
 console.log(roundString2('Write')); // write
 console.log(roundString2('PHp')); // PHP
 console.log(roundString2('HELLO world')); // PHP
+
+// make a decimal number under 1000 into a binary number
+function toBinary(number) {
+    let binaryNumber = '';
+    for (let i = 9; i > -1; i--) {
+        if (number >= Math.pow(2, i)) {
+            binaryNumber += '1';
+            number = number - Math.pow(2, i);
+        } else {
+            binaryNumber += '0';
+        }
+    }
+    return Number(binaryNumber);
+}
+console.log(toBinary(123));
+console.log(toBinary(1023));
+console.log(toBinary(682));
+
+function toDecimal(binaryNumber2) {
+    let binaryNumber = binaryNumber2.toString();
+    let decimalNumber = 0;
+    const last = binaryNumber.length-1; // 9
+    //console.log(last);
+    for (let i = 0; i < binaryNumber.length; i++) {
+        //console.log('last char: ' + binaryNumber.charAt(last-i));
+        if (binaryNumber.charAt(last-i) === '1') {
+            //console.log(decimalNumber);
+            decimalNumber += Math.pow(2, i);
+        }
+    }
+    return decimalNumber;
+}
+console.log(toDecimal(1010101010)); //682
+console.log(toDecimal(1111111111)); //1023
+
+// REAL LIFE SCENARIOS 
+// GEOCACHING 
+// PUZZLE: 
+
