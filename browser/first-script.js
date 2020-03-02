2c213e681cc058c540d5307b31082ecdba4daabc
Martina Freundorfer <martina.freundorfer@digitalcareerinstitute.org>
2020-03-02 14:37:00 +0100

    hi

--- browser/first-script.js
+++ browser/first-script.js
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 /* eslint-disable eol-last */
 /* eslint-disable semi */
 const inlineJavascript = 'hello world';
@@ -6,5 +7,37 @@ console.log(inlineJavascript);
 console.log(window);
 console.log({});
 
-const name = prompt('What is your name?'); // asking for information
-alert('Hello World ' + name); // giving information
+//const name = prompt('What is your name?'); // asking for information
+//alert('Hello World ' + name); // giving information
+
+// WET PRINCIPLE NOT OPTIMIZED :)
+function game() {
+    const randomNumber = Math.floor(Math.random() * 10 + 1);
+    console.log(randomNumber);
+    let inputNumber = prompt('Guess a number between 1 - 10:');
+    let attempts = 1;
+    //console.log(typeof(inputNumber));
+    //console.log(typeof(randomNumber));
+    if (randomNumber === parseInt(inputNumber)) {
+        alert('Success, the number was ' + randomNumber + '! Attempts: ' + attempts);
+    } else {
+        attempts++;
+        inputNumber = prompt('Nope, sorry!!! Try again:');
+        if (randomNumber === parseInt(inputNumber)) {
+            alert('Success, the number was ' + randomNumber + '! Attempts: ' + attempts);
+        } else {
+            attempts++;
+            inputNumber = prompt('Nope, sorry!!! Try again:');
+            if (randomNumber === parseInt(inputNumber)) {
+                alert('Success, the number was ' + randomNumber + '! Attempts: ' + attempts);
+            } else {
+                alert('Nope, sorry!!! the correct number was ' + randomNumber);
+            }
+        }
+    }
+}
+console.log(document);
+console.log(document.head);
+console.log(document.title);
+let title = document.getElementById('websiteTitle');
+console.log(title);
