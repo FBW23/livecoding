0d3fd33ee9adce7d75ca788e62438622cfc73cb5
Martina Freundorfer <martina.freundorfer@digitalcareerinstitute.org>
2020-03-12 13:30:20 +0100

    hello

--- browser/keypress.js
+++ browser/keypress.js
@@ -1,7 +1,5 @@
 const newsletterBox = document.querySelector('#newsletter');
-
 //console.log(newsletterBox);
-
 const hideFunction = () => {
     // console.log('keydown');
     // console.log(event);
@@ -10,12 +8,30 @@ const hideFunction = () => {
         newsletterBox.style.display = 'none';
     }
 };
-
 const showFunction = () => {
+    console.log(event.target);
     if (event.keyCode === 13) { // 13 enter Key?
         newsletterBox.style.display = 'block';
     }
 };
+// better than on the document only
+document.body.addEventListener('keydown', hideFunction);
+document.body.addEventListener('keyup', showFunction);
 
-document.addEventListener('keydown', hideFunction);
-document.addEventListener('keyup', showFunction);
+const form = document.querySelector('form');
+
+const mySubmit = () => {
+    // stop the default behaviour of reloaded
+    event.preventDefault();
+    console.log('You entered the email adress ' + event.target.elements['email'].value);
+}
+
+form.addEventListener('submit', mySubmit);
+
+const myReset = () => {
+    // stop the default behaviour of reloaded
+    event.preventDefault();
+    console.log('hello reset');
+}
+
+form.addEventListener('reset', myReset);
